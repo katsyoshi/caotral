ec2ab4ab15158560a8271f314b6717cd30d4f437
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2023-03-06 10:29:58 +0900

    add compile method

--- lib/vaporware.rb
+++ lib/vaporware.rb
@@ -5,4 +5,10 @@ require_relative "vaporware/version"
 module Vaporware
   class Error < StandardError; end
   # Your code goes here...
+  class Compiler
+    attr_reader :source, :ast
+    def initialize(source = nil) = @source = source
+    def self.compile(source) = new(source).compile
+    end
+  end
 end
