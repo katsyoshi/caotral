2efb10dea5207c6949f8a1978b7c4a0fda5b167b
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2023-03-22 15:34:33 +0900

    use test-unit.gem

--- test/test.rb
+++ test/test.rb
@@ -1,27 +1,27 @@
 require "vaporware"
+require "test/unit"
 
-class VaporwareTest
-  def initialize(file, debug = false)
-    @vaporware = Vaporware::Compiler.new(file, debug:)
-    @debug = debug
-  end
+class VaporwareTest < Test::Unit::TestCase
+  def teardown = File.delete("tmp")
 
   def test_sample_plus
+    @file = "sample/plus.rb"
+    @vaporware = Vaporware::Compiler.new(@file)
     @vaporware.compile
     IO.popen("./tmp").close
     exit_code, handle_code = check_process($?.to_i)
-    puts [exit_code == 9, handle_code == 0].all?
-    puts exit_code, handle_code if @debug
-    File.delete("tmp") unless @debug
+    assert_equal(9, exit_code)
+    assert_equal(0, handle_code)
   end
 
   def test_sample_variable
+    @file = "sample/variable.rb"
+    @vaporware = Vaporware::Compiler.new(@file)
     @vaporware.compile
     IO.popen("./tmp").close
     exit_code, handle_code = check_process($?.to_i)
-    puts [exit_code == 1, handle_code == 0].all?
-    puts exit_code, handle_code if @debug
-    File.delete("tmp") unless @debug
+    assert_equal(1, exit_code)
+    assert_equal(0, handle_code)
   end
 
   private
@@ -33,7 +33,3 @@ class VaporwareTest
     ]
   end
 end
-
-debug = ARGV.shift
-VaporwareTest.new("sample/plus.rb", debug).test_sample_plus
-VaporwareTest.new("sample/variable.rb", debug).test_sample_variable
--- vaporware.gemspec
+++ vaporware.gemspec
@@ -26,6 +26,7 @@ Gem::Specification.new do |spec|
 
   # Uncomment to register a new dependency of your gem
   spec.add_dependency "parser"
+  spec.add_development_dependency "test-unit"
 
   # For more information and examples about making a new gem, check out our
   # guide at: https://bundler.io/guides/creating_gem.html
