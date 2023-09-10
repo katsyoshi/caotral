092e97b60d733bb46e2e4aa7f2a5733b8ecec477
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2023-09-10 12:52:00 +0900

    link shared library linking object files

--- lib/vaporware/compiler.rb
+++ lib/vaporware/compiler.rb
@@ -10,7 +10,7 @@ class Vaporware::Compiler
     s.compile(compiler_options:)
     obj_file = s.assemble(input: _precompile, assembler: "as", debug:)
     output = File.basename(obj_file, ".o")
-    output = "lib#{output}.so" if shared
+    output = "lib#{output}.so" if shared && output !~ /^lib.+\.so$/
     s.link(input: obj_file, output:, shared:)
     File.delete(obj_file) unless debug
     File.delete(_precompile) unless debug
@@ -34,7 +34,8 @@ class Vaporware::Compiler
   def link(input:, output: File.basename(input, ".*"), linker: "mold", linker_options: [], dyn_ld_path: ["-dynamic-linker", "/lib64/ld-linux-x86-64.so.2"], ld_path: ["/lib64/libc.so.6", "/usr/lib64/crt1.o"], shared: false)
     if shared
       dyn_ld_path = []
-      ld_path = ["/usr/lib64/crti.o", "/usr/lib/gcc/x86_64-pc-linux-gnu/13/crtbeginS.o"]
+      ld_path = ["/usr/lib64/crti.o", "/usr/lib/gcc/x86_64-pc-linux-gnu/13/crtbeginS.o", "/usr/lib/gcc/x86_64-pc-linux-gnu/13/crtendS.o", "/usr/lib64/crtn.o",]
+
       linker_options = ["-shared"]
     end
     linker_commands = [linker, *linker_options, *dyn_ld_path, "-o", output, *ld_path, input].compact
