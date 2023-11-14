bbb83d22a8444c30508dd0104a65e53f2130168b
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2023-11-14 21:03:19 +0900

    fix split to scan in hexas method

--- lib/vaporware/compiler/assembler/elf/utils.rb
+++ lib/vaporware/compiler/assembler/elf/utils.rb
@@ -6,7 +6,7 @@ module Vaporware::Compiler::Assembler::ELF::Utils
   private
   def align(val, bytes) = (val << 0 until val.size % bytes == 0)
   def bytes = (raise Vaporware::Compiler::Assembler::ELF::Error, "should be implement #{self.class}")
-  def num2bytes(val, bytes) = hexas(val, bytes*2).reverse
-  def check(val, bytes) = ((val.is_a?(Array) && val.all? { |v| v.is_a?(Integer) } && val.size == bytes) || (val.is_a?(Integer) && (hexas(val, bytes*2).size == bytes)))
-  def hexas(val, hex) = ("%0#{hex}x" % val).split(/.{1,2/).map { |v| v.to_i(16) }
+  def num2bytes(val, bytes) = hexas(val, bytes).reverse
+  def check(val, bytes) = ((val.is_a?(Array) && val.all? { |v| v.is_a?(Integer) } && val.size == bytes) || (val.is_a?(Integer) && (hexas(val, bytes).size == bytes)))
+  def hexas(val, hex) = ("%0#{hex*2}x" % val).scan(/.{1,2}/).map { |v| v.to_i(16) }.then { |list| list.unshift(0) until list.size >= hex; list }
 end
