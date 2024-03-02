0fdd912f595e1fe0be4ddeba23104da2a80a2389
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2024-03-02 22:51:56 +0900

    add shstrtab for twice

--- test/vaporware/compiler/assembler/elf/section/test_shstrtab.rb
+++ test/vaporware/compiler/assembler/elf/section/test_shstrtab.rb
@@ -4,9 +4,11 @@ require "test/unit"
 class Vaporware::Compiler::Assembler::ELF::Section::TestShstrtab < Test::Unit::TestCase
   def setup = @shstrtab = Vaporware::Compiler::Assembler::ELF::Section::Shstrtab.new
   def test_set_values
-    binary = "\x00.main\x00".force_encoding("ASCII-8BIT")
-    @shstrtab.set!(name: "main")
-
+    binary = "\x00.text\x00".force_encoding("ASCII-8BIT")
+    @shstrtab.set!(name: "text")
+    assert_equal(@shstrtab.build, binary)
+    binary = "\x00.text\x00.strtab\x00".force_encoding("ASCII-8BIT")
+    @shstrtab.set!(name: "strtab")
     assert_equal(@shstrtab.build, binary)
   end
   def test_alert_values
