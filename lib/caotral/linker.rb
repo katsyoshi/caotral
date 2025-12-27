2bacf06075e89bb7067c98f7eb6b42c2d9de6680
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2025-12-27 12:15:24 +0900

    add linker elf stubs

--- lib/caotral/linker.rb
+++ lib/caotral/linker.rb
@@ -12,6 +12,8 @@ class Caotral::Linker
 
   def link_command(input: @input, output: @output, debug: @debug, shared: @shared)
     ld_path = []
+    return to_elf(input:, output:, debug:) if @linker == "self"
+
     if @shared
       ld_path << "--shared"
       ld_path << "#{libpath}/crti.o"
@@ -36,4 +38,6 @@ class Caotral::Linker
 
   def libpath = @libpath ||= File.dirname(Dir.glob("/usr/lib*/**/crti.o").last)
   def gcc_libpath = @gcc_libpath ||= File.dirname(Dir.glob("/usr/lib/gcc/x86_64-*/*/crtbegin.o").last)
+
+  def to_elf(input: @input, output: @output, debug: @debug) = Caotral::Linker::ELF.link!(input:, output:, debug:)
 end
---
+++ lib/caotral/linker/elf/reader.rb
@@ -0,0 +1,8 @@
+
+class Caotral::Linker::ELF::Reader
+  def read!(input:, output:, debug: false)
+  end
+
+  def initialize(input:, output:, debug: false, linker_options: [])
+  end
+end
