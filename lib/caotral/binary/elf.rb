e47df6f18f73d22be3a9d16a4aa27c8f5479f242
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-01-06 21:22:06 +0900

    adjust elf section storage

--- lib/caotral/binary/elf.rb
+++ lib/caotral/binary/elf.rb
@@ -4,6 +4,15 @@ require_relative "elf/error"
 module Caotral
   class Binary
     class ELF
+      include Enumerable
+      attr_reader :sections, :header
+      def initialize
+        @sections = []
+        @header = nil
+      end
+      def each(&block) = @sections.each(&block)
+      def [](idx) = @sections[idx]
+      def select_by_name(section_name) = @sections.select { [it.section_name, it.name].include?(section_name.to_s) }
     end
   end
 end
