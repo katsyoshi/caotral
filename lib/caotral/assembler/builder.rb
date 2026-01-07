71edbe22be7bdd01853645a0f86890fa5c986c86
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-01-07 21:47:46 +0900

    refactoring section definitions

--- lib/caotral/assembler/builder.rb
+++ lib/caotral/assembler/builder.rb
@@ -10,7 +10,23 @@ module Caotral
       def build
         elf = Caotral::Binary::ELF.new
         elf.header = Caotral::Binary::ELF::Header.new
+
+        [
+          [nil, nil],
+          [".text", assemble(@instructions)],
+          [".strtab", Caotral::Binary::ELF::Section::Strtab.new],
+          [".symtab", Caotral::Binary::ELF::Section::Symtab.new],
+          [".shstrtab", Caotral::Binary::ELF::Section::Strtab.new],
+        ].each do |(section_name, body)|
+          header = Caotral::Binary::ELF::SectionHeader.new
+          section = Caotral::Binary::ELF::Section.new(header:, body:, section_name:)
+          elf.sections << section
+        end
         elf
+      end
+
+      private def assemble(instructions)
+        Caotral::Assembler::Builder::Text.new(instructions:).build
       end
     end
   end
