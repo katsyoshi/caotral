4b39fb203e2b41f16367eb70fa3583e0716bcb72
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-01-10 23:44:28 +0900

    steep sensei

--- lib/caotral/assembler/builder.rb
+++ lib/caotral/assembler/builder.rb
@@ -11,13 +11,13 @@ module Caotral
         elf = Caotral::Binary::ELF.new
         elf.header = Caotral::Binary::ELF::Header.new
 
-        [
-          [nil, nil],
-          [".text", assemble_text(@instructions)],
-          [".strtab", Caotral::Binary::ELF::Section::Strtab.new],
-          [".symtab", Caotral::Binary::ELF::Section::Symtab.new],
-          [".shstrtab", Caotral::Binary::ELF::Section::Strtab.new],
-        ].each do |(section_name, body)|
+        sections = []
+        sections << [nil, nil]
+        sections << [".text", assemble_text(@instructions)]
+        sections << [".strtab", Caotral::Binary::ELF::Section::Strtab.new]
+        sections << [".symtab", Caotral::Binary::ELF::Section::Symtab.new]
+        sections << [".shstrtab", Caotral::Binary::ELF::Section::Strtab.new]
+        sections.each do |(section_name, body)|
           header = Caotral::Binary::ELF::SectionHeader.new
           section = Caotral::Binary::ELF::Section.new(header:, body:, section_name:)
           elf.sections << section
