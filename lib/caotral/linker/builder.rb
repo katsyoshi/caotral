88667953b0396ff8507861422e72c60ec84cf14d
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-02-13 21:45:55 +0900

    initialize .got.plt with null entry

--- lib/caotral/linker/builder.rb
+++ lib/caotral/linker/builder.rb
@@ -53,7 +53,7 @@ module Caotral
                     .set!(type: SHT[:progbits], flags: SHF[:ALLOC] | SHF[:EXECINSTR], addralign: 16)
         )
         got_plt_section = Caotral::Binary::ELF::Section.new(
-          body: String.new,
+          body: [0].pack("Q<"),
           section_name: ".got.plt",
           header: Caotral::Binary::ELF::SectionHeader.new
                     .set!(type: SHT[:progbits], flags: SHF[:ALLOC] | SHF[:WRITE], addralign: 8)
