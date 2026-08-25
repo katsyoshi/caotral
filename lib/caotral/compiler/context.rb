c8584d4225ff6b2264c1d28e0d090e8a51631f5a
MATSUMOTO, Katsuyoshi <github@katsyoshi.org>
2026-08-26 00:39:56 +0900

    Extract compiler variables context

--- lib/caotral/compiler/context.rb
+++ lib/caotral/compiler/context.rb
@@ -1,22 +1,25 @@
+require_relative "context/variables"
+
 module Caotral
   class Compiler
     class Context
-      attr_reader :label_sequence, :discovered_methods, :local_variables
+      attr_reader :discovered_methods, :label_sequence
       def initialize
         @label_sequence = 0
         @entry_emitted = false
         @emitted_methods = Set[]
         @discovered_methods = Set[]
-        @local_variables = Set[]
+        @variables = Caotral::Compiler::Context::Variables.new
       end
 
       def entry_emitted? = @entry_emitted
       def mark_entry_emitted = @entry_emitted = true
       def mark_method_emitted(name) = @emitted_methods << name
       def discover_method(name) = @discovered_methods << name
-      def register_local_variable(name) = @local_variables << name
+      def register_local_variable(name) = @variables.register_local(name)
       def all_methods_emitted? = @discovered_methods == @emitted_methods
       def increment_label_sequence = @label_sequence += 1
+      def local_variables = @variables.locals
     end
   end
 end
